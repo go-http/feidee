4de27d2b97ff31d55b90f69503e4184c01a4f836
Athurg Feng <athurg@gooth.org>
2017-08-14 12:47:45 +0800

    增加使用范例：账本导出工具

---
+++ example/exporter/main.go
@@ -0,0 +1,46 @@
+//随手记导出工具
+package main
+
+import (
+	"../.."
+
+	"flag"
+	"log"
+	"time"
+)
+
+func main() {
+	var username, password, accountBook, filename string
+
+	flag.StringVar(&username, "u", "", "用户名/邮箱地址")
+	flag.StringVar(&password, "p", "", "密码")
+	flag.StringVar(&accountBook, "b", "默认账本", "账本名")
+	flag.StringVar(&filename, "f", time.Now().Format("2006-01-02.xls"), "保存的文件名")
+	flag.Parse()
+
+	if username == "" || password == "" {
+		flag.Usage()
+		return
+	}
+
+	client := feidee.New()
+	err := client.Login(username, password)
+	if err != nil {
+		log.Fatalf("登录失败:%s", err)
+	}
+
+	err = client.SyncAccountBookList()
+	if err != nil {
+		log.Fatalf("刷新账本列表失败: %s", err)
+	}
+
+	err = client.SwitchBook(accountBook)
+	if err != nil {
+		log.Fatalf("切换账本失败: %s", err)
+	}
+
+	err = client.ExportToFile(filename)
+	if err != nil {
+		log.Fatalf("下载失败%s", err)
+	}
+}
